9382ea898ab83856168ad74f86f96740aca35798
teddu0 <107142125+teddu0@users.noreply.github.com>
2022-06-13 17:14:14 +0300

    Добавили функции по вычислению площади квадрата и прямоугольника, разобрались как работают функции Java

--- src/main/java/SecondStep.java
+++ src/main/java/SecondStep.java
@@ -1,7 +1,26 @@
 public class SecondStep {
     public static void main(String[] args) {
-        int l = 4;
-        int s = l * l;
-        System.out.println("Площать квадрата со стороные " + l + " = " + s);
+        hello("world");
+
+        int len = 4;
+        System.out.println("Площадь квадрата со сторой " + len + " = " + area(len));
+
+        int a = 2;
+        int b = 4;
+        System.out.println("Площадь прямоугольника, со сторонами " + a + " и " + b + " = "  + area(a, b));
     }
+
+    public static void hello (String somebody) {
+        System.out.println("Hello, " + somebody);
+    }
+
+
+    public static int area (int len){
+        return len * len;
+    }
+
+    public static int area (int a, int b) {
+        return a * b;
+    }
+
 }
